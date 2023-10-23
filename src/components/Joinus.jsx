c3f5b8a95d3d14950f9d4ec5e1cb9bedda372aad
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-23 19:44:44 +0530

    Update Joinus.jsx

--- src/components/Joinus.jsx
+++ src/components/Joinus.jsx
@@ -1,34 +1,42 @@
-import React from 'react'
-import {FaTelegramPlane} from 'react-icons/fa'
+import React from "react";
+import { FaTelegramPlane } from "react-icons/fa";
 
 function Joinus() {
   return (
     <section>
-<div className='container-wrapper'>
-  <div className=' relative'>
-    <img src="images/foot-up.png" alt="" className='hidden sm:block'/>
-    <img src="images/mob-foot-up.png" alt="" className='block sm:hidden'/>
-    
-<div className='conted absolute sm:top-0 top-[24%] sm:left-[14%] left-[5%] px-[5%]'>
-  <div className=' text-center'>
-    <h1 className=' text-xl sm:text-[65px] font-semibold'>Join Us To Make The World <br /> A
-Better Place</h1>
-<p className='lh-1_6 text-base sm:text-xl'>
-We are willing to listen to the community's opinions, for the purpose <br /> of 
-developing useful products for users.
-</p>
-<div className=' flex gap-5 text-center justify-center mt-8'>
-  <FaTelegramPlane className=' text-4xl sm:text-5xl'/>
-  <a href="/" className=' bg-white py-3 text-sm sm:text-2xl text-black font-semibold px-6 rounded-md '>Join our Community</a>
-</div>
-  </div>
+      <div className="container-wrapper">
+        <div className=" relative">
+          <img src="images/foot-up.png" alt="" className="hidden sm:block" />
+          <img
+            src="images/mob-foot-up.png"
+            alt=""
+            className="block sm:hidden"
+          />
 
-</div>
-  </div>
-
-</div>
+          <div className="conted absolute sm:top-[14%] top-[24%] sm:left-[9%] left-[5%] px-[5%]">
+            <div className=" text-center">
+              <h1 className=" text-xl sm:text-[65px] font-semibold lh-1_4">
+                Join Us To Make The World <br /> A Better Place
+              </h1>
+              <p className="lh-1_6 text-base sm:text-xl">
+                We are willing to listen to the community's opinions, for the
+                purpose <br /> of developing useful products for users.
+              </p>
+              <div className=" flex gap-5 text-center justify-center mt-8">
+                <FaTelegramPlane className=" text-4xl sm:text-5xl" />
+                <a
+                  href="/"
+                  className=" bg-white py-3 text-sm sm:text-2xl text-black font-semibold px-6 rounded-md "
+                >
+                  Join our Community
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Joinus
+export default Joinus;
