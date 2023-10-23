c77e5ee22c9b9605151d379abe189f2aa26b2d70
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-23 18:11:18 +0530

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,40 @@ function Footer() {
     <div className=' absolute'>
       <img src="images/foot-logo.png" alt="" className='h-[130px]' />
     </div>
-    <div>
+    <div className=' relative'>
       <img src="images/footer.png" alt="" />
+      <div className=' absolute top-[15%] w-[1000px] left-[14%]'>
+        <div className='container-wrapper'>
+        <div className='flex justify-around'>
+          <div className=' flex flex-col'>
+            <a href="/" className=' text-xl font-medium lh-1_9 mb-4'>Whitepaper</a>
+            <a href="/" className=' text-xl font-medium lh-1_9 mb-4'>About us</a>
+            <a href="/" className=' text-xl font-medium lh-1_9'>Contact us</a>
+          </div>
+
+          <div className=' flex flex-col'>
+          <a href="/" className=' text-xl font-medium lh-1_9 mb-4'>Mikawa Inu EcoSystem</a>
+          <a href="/" className=' text-xl font-medium lh-1_9 mb-4'>Web 3.0 Platform</a>    
+          <a href="/" className=' text-xl font-medium lh-1_9'>NFTs MarketPlace</a>
+          </div>
+
+          <div className=' flex flex-col'>
+          <a href="/" className=' text-xl font-medium lh-1_9 mb-4'>Mikawa - Chain</a>
+          <a href="/" className=' text-xl font-medium lh-1_9 mb-4'>Tokenomics</a>
+          <a href="/" className=' text-xl font-medium lh-1_9'>Wallet development</a>
+          </div>
+
+       
+        </div>
+        <div className=' flex justify-around mt-[8rem] items-center'>
+          <p className=' text-base font-medium'>All Rights Reserved 2022</p>
+          <img src="images/audit.png" alt="" />
+
+        </div>
+        
+
+        </div>
+      </div>
     </div>
       </div>
 
