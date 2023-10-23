ca23d17c03c96095dcac0818ea5f88188c778419
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-23 19:38:41 +0530

    new

--- src/components/Joinus.jsx
+++ src/components/Joinus.jsx
@@ -9,11 +9,11 @@ function Joinus() {
     <img src="images/foot-up.png" alt="" className='hidden sm:block'/>
     <img src="images/mob-foot-up.png" alt="" className='block sm:hidden'/>
     
-<div className='conted absolute top-0 left-[14%]'>
+<div className='conted absolute sm:top-0 top-[24%] sm:left-[14%] left-[5%] px-[5%]'>
   <div className=' text-center'>
-    <h1 className=' text-2xl sm:text-[65px] font-semibold'>Join Us To Make The World <br /> A
+    <h1 className=' text-xl sm:text-[65px] font-semibold'>Join Us To Make The World <br /> A
 Better Place</h1>
-<p className='lh-1_6 text-xl'>
+<p className='lh-1_6 text-base sm:text-xl'>
 We are willing to listen to the community's opinions, for the purpose <br /> of 
 developing useful products for users.
 </p>
