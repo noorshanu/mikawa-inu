b458fb0a043c7ac7ef908ab551703cb6ad3176d8
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-23 20:02:43 +0530

    Update Ecosystem.jsx

--- src/components/Ecosystem.jsx
+++ src/components/Ecosystem.jsx
@@ -4,18 +4,18 @@ function Ecosystem() {
   return (
     <section className="pt-10">
       <div className="container-wrapper">
-        <div className=" flex justify-between ">
+        <div className=" flex justify-between flex-col sm:flex-row ">
           <div>
             <span className="bg-white px-4 py-2 text-black rounded-lg text-lg font-medium font-monot ">
               Features
             </span>
             <div className="flex gap-4 mt-5">
               <img src="images/strok.png" alt="" className=" sm:h-auto h-[100px]" />
-              <h3 className="text-[#E476B2] text-2xl sm:text-6xl font-semibold lh-1_2">Mikawa Inu Ecosystem</h3>
+              <h3 className="text-[#E476B2] text-2xl sm:text-6xl font-semibold lh-1_2">Mikawa <br /> Inu <br /> Ecosystem</h3>
             </div>
           </div>
           <div>
-            <img src="images/eco-1.png" alt="" className=" mt-[4rem]" />
+            <img src="images/eco-1.png" alt="" className=" sm:mt-[4rem]" />
           </div>
         </div>
       </div>
