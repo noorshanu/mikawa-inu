b40124c1bb85239c01f870c633b40b5ebd83f2d6
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-25 14:24:44 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,19 +2,18 @@ import React from "react";
 
 function Hero() {
   return (
-    <section className=" pt-8">
+    <section className=" pt-12">
       <div className="container-wrapper">
-        <div className="flex flex-col sm:flex-row justify-between gap-5">
+        <div className="flex flex-col sm:flex-row justify-between gap-10">
           <div data-aos="fade-left"
      data-aos-offset="300" data-aos-duration="2000" 
      data-aos-easing="ease-in-sine">
-            <h1 className="font-monot text-3xl sm:text-7xl font-bold">
-              Digital Economy <br />
-              <span className=" text-purple-600">The Power</span> Of Technology
+            <h1 className="font-monot text-3xl sm:text-6xl font-bold lh-1_3 mb-5">
+              Digital Economy <span className=" text-pink-400">The Power</span> <br /> Of Technology
             </h1>
-            <p className=" text-sm sm:text-lg font-normal">
-              Change The World Of Traditional Finance By Creating A Trusted
-              Decentralized <br /> Platform Powered By Artificial Intelligence
+            <p className=" text-sm sm:text-base font-normal font-monot">
+              Change The World Of Traditional Finance By Creating A Trusted<br /> 
+              Decentralized Platform Powered By Artificial Intelligence <br />
               To Analyze Wealth Management Opportunities.
             </p>
             <div className=" flex gap-6 mt-5"> 
