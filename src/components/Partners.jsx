796060693f3104c523fdfa91a2f50f905b4cd095
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-27 02:07:11 +0530

    new

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -5,15 +5,19 @@ function Partners() {
   return (
     <section className=' py-5'>
  <Marquee>
-  <img src="images/coin.png" alt="" />
-  <img src="images/coin2.png" alt="" />
-  <img src="images/inter.png" alt="" />
-  <img src="images/nerve.png" alt="" />
+  <img src="images/coin.png" alt="" className='h-[84px] px-6' />
+  <img src="images/coin2.png" alt="" className='h-[39px] px-6' />
+  <img src="images/houbi.png" alt="" className='h-[42px] px-6' />
+  <img src="images/nerve.png" alt="" className='h-[45px] px-6' />
+  <img src="https://gimg2.gateimg.com/LOGO/1653358199192520467_downlod_gate2.png" alt=""  className='h-[45px] px-6'/>
+  <img src="images/defi.webp" alt=""  className='h-[48px] px-6'/>
 
-  <img src="images/coin.png" alt="" />
-  <img src="images/coin2.png" alt="" />
-  <img src="images/inter.png" alt="" />
-  <img src="images/nerve.png" alt="" />
+  <img src="images/coin.png" alt="" className='h-[84px] px-6' />
+  <img src="images/coin2.png" alt="" className='h-[39px] px-6' />
+  <img src="images/houbi.png" alt="" className='h-[42px] px-6'/>
+  <img src="images/nerve.png" alt=""  className='h-[45px] px-6'/>
+  <img src="https://gimg2.gateimg.com/LOGO/1653358199192520467_downlod_gate2.png" alt=""  className='h-[45px] px-6'/>
+  <img src="images/defi.webp" alt=""  className='h-[48px] px-6'/>
 
   </Marquee>
 
